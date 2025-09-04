7c6f7d4fc7022e79a017326785597787a7b3e223
mvamshireddy <98573876+mvamshireddy@users.noreply.github.com>
2025-09-04 16:09:51 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const { sequelize } = require('./config/db');
-const PORT = process.env.PORT || 5000;
-
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
